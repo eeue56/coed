556cf3dc7a59488e64c8f63e4acb63d935f26480
eeue56 <noah.hall@hey.com>
2026-06-27 16:47:17 +0200

    js: array literal tokenization

--- src/langs/javascript/tokenize.ts
+++ src/langs/javascript/tokenize.ts
@@ -60,25 +60,64 @@ function isNumberPart(char: string, buffer: string): boolean {
     return false;
 }
 
+function isLeftBracket(char: string): boolean {
+    return char === "[";
+}
+
+function isRightBracket(char: string): boolean {
+    return char === "]";
+}
+
+function isComma(char: string): boolean {
+    return char === ",";
+}
+
 type TokenizerModel = {
-    state: TokenizerState;
+    state: BufferingTokenizerState;
     currentTokenStartIndex: number;
     buffer: string;
 };
 
-type TokenizerState =
+type OneOffTokenizerState =
+    | "ReadLeftBracket"
+    | "ReadRightBracket"
+    | "ReadComma";
+
+function isOneOffTokenizerState(
+    state: TokenizerState,
+): state is OneOffTokenizerState {
+    return (
+        state === "ReadLeftBracket" ||
+        state === "ReadRightBracket" ||
+        state === "ReadComma"
+    );
+}
+
+type BufferingTokenizerState =
     | "ReadyForNextToken"
     | "ReadingNumber"
     | "ReadingIdentifier"
     | "ReadingString";
 
+function isBufferingTokenizerState(
+    state: TokenizerState,
+): state is BufferingTokenizerState {
+    return (
+        state === "ReadyForNextToken" ||
+        state === "ReadingNumber" ||
+        state === "ReadingIdentifier" ||
+        state === "ReadingString"
+    );
+}
+
+type TokenizerState = BufferingTokenizerState | OneOffTokenizerState;
+
 function switchTokenizerState(
     newState: TokenizerState,
     tokenizerModel: TokenizerModel,
     tokens: Token[],
+    currentIndex: number,
 ): void {
-    const start = tokenizerModel.currentTokenStartIndex;
-    const endIndex = start + tokenizerModel.buffer.length;
     const currentState = tokenizerModel.state;
 
     console.log("setting current state to: ", newState);
@@ -87,10 +126,65 @@ function switchTokenizerState(
         return;
     }
 
+    if (isOneOffTokenizerState(newState)) {
+        if (isBufferingTokenizerState(currentState)) {
+            console.log("clearing buffered state");
+            // clear out the old buffer
+            switchTokenizerState(
+                "ReadyForNextToken",
+                tokenizerModel,
+                tokens,
+                currentIndex,
+            );
+        }
+
+        const start = currentIndex;
+        console.log("Handling one-off state: ", newState);
+        switch (newState) {
+            case "ReadLeftBracket": {
+                console.log("ReadLeftBracket, pushing LeftBracketToken");
+                tokens.push({
+                    kind: "LeftBracketToken",
+                    startIndex: start,
+                    endIndex: start + 1,
+                });
+                break;
+            }
+            case "ReadRightBracket": {
+                tokens.push({
+                    kind: "RightBracketToken",
+                    startIndex: start,
+                    endIndex: start + 1,
+                });
+                break;
+            }
+            case "ReadComma": {
+                tokens.push({
+                    kind: "CommaToken",
+                    startIndex: start,
+                    endIndex: start + 1,
+                });
+                break;
+            }
+        }
+
+        console.log("Done with one-off state, resetting to ReadyForNextToken");
+
+        tokenizerModel.state = "ReadyForNextToken";
+        tokenizerModel.buffer = "";
+        return;
+    }
+
+    console.log("Going to full validation for state: ", currentState);
+
+    const start = tokenizerModel.currentTokenStartIndex;
+    const endIndex = start + tokenizerModel.buffer.length;
+
     switch (currentState) {
-        case "ReadyForNextToken":
+        case "ReadyForNextToken": {
             break;
-        case "ReadingNumber":
+        }
+        case "ReadingNumber": {
             tokens.push({
                 kind: "NumberToken",
                 value: parseFloat(tokenizerModel.buffer),
@@ -98,7 +192,8 @@ function switchTokenizerState(
                 endIndex: endIndex,
             });
             break;
-        case "ReadingIdentifier":
+        }
+        case "ReadingIdentifier": {
             tokens.push({
                 kind: "IdentifierToken",
                 name: tokenizerModel.buffer,
@@ -106,7 +201,8 @@ function switchTokenizerState(
                 endIndex: endIndex,
             });
             break;
-        case "ReadingString":
+        }
+        case "ReadingString": {
             tokens.push({
                 kind: "StringToken",
                 value: tokenizerModel.buffer,
@@ -114,6 +210,7 @@ function switchTokenizerState(
                 endIndex: endIndex,
             });
             break;
+        }
     }
 
     tokenizerModel.state = newState;
@@ -138,14 +235,18 @@ export function tokenize(string: string): Token[] {
         if (tokenizerModel.state === "ReadingString") {
             const openingQuote =
                 tokenizerModel.buffer[tokenizerModel.currentTokenStartIndex];
-            console.log("char", char, "openingQuote", openingQuote);
+
             if (char === openingQuote) {
                 tokenizerModel.buffer += char;
                 switchTokenizerState(
                     "ReadyForNextToken",
                     tokenizerModel,
                     tokens,
+                    i,
                 );
+                continue;
+            } else {
+                tokenizerModel.buffer += char;
             }
         } else if (tokenizerModel.state === "ReadingNumber") {
             if (!isNumberPart(char, tokenizerModel.buffer)) {
@@ -153,24 +254,78 @@ export function tokenize(string: string): Token[] {
                     "ReadyForNextToken",
                     tokenizerModel,
                     tokens,
+                    i,
                 );
-            }
-        } else {
-            if (isStringQuote(char)) {
-                switchTokenizerState("ReadingString", tokenizerModel, tokens);
-                tokenizerModel.currentTokenStartIndex = i;
-            } else if (isNumberStart(char)) {
-                switchTokenizerState("ReadingNumber", tokenizerModel, tokens);
-                tokenizerModel.currentTokenStartIndex = i;
+            } else {
+                tokenizerModel.buffer += char;
             }
         }
 
-        tokenizerModel.buffer += char;
+        if (tokenizerModel.state === "ReadyForNextToken") {
+            if (isStringQuote(char)) {
+                switchTokenizerState(
+                    "ReadingString",
+                    tokenizerModel,
+                    tokens,
+                    i,
+                );
+                tokenizerModel.currentTokenStartIndex = i;
+                tokenizerModel.buffer += char;
+            } else if (isNumberStart(char)) {
+                switchTokenizerState(
+                    "ReadingNumber",
+                    tokenizerModel,
+                    tokens,
+                    i,
+                );
+                tokenizerModel.currentTokenStartIndex = i;
+                tokenizerModel.buffer += char;
+            } else if (isLeftBracket(char)) {
+                switchTokenizerState(
+                    "ReadLeftBracket",
+                    tokenizerModel,
+                    tokens,
+                    i,
+                );
+                tokenizerModel.currentTokenStartIndex = i;
+                console.log("Now state: ", tokenizerModel.state);
+                console.log("now model", tokenizerModel);
+            } else if (isRightBracket(char)) {
+                switchTokenizerState(
+                    "ReadRightBracket",
+                    tokenizerModel,
+                    tokens,
+                    i,
+                );
+                tokenizerModel.currentTokenStartIndex = i;
+            } else if (isComma(char)) {
+                switchTokenizerState("ReadComma", tokenizerModel, tokens, i);
+                tokenizerModel.currentTokenStartIndex = i;
+            } else if (isIdentifierStart(char)) {
+                switchTokenizerState(
+                    "ReadingIdentifier",
+                    tokenizerModel,
+                    tokens,
+                    i,
+                );
+                tokenizerModel.currentTokenStartIndex = i;
+                tokenizerModel.buffer += char;
+            } else {
+                tokenizerModel.buffer += char;
+            }
+        }
     }
 
     if (tokenizerModel.state !== "ReadyForNextToken") {
-        switchTokenizerState("ReadyForNextToken", tokenizerModel, tokens);
+        switchTokenizerState(
+            "ReadyForNextToken",
+            tokenizerModel,
+            tokens,
+            string.length - 1,
+        );
     }
+
+    console.log("final tokens: ", tokens);
 
     return tokens;
 }
--- src/tests/langs/javascript/tokenize_test.ts
+++ src/tests/langs/javascript/tokenize_test.ts
@@ -61,18 +61,33 @@ export function testFloatNumber() {
     ]);
 }
 
-export function testExponentNumberNotSupported() {
-    const tokens = tokenize("1.2e3");
+export function testArrayLiteral() {
+    const tokens = tokenize("[1,2]");
     assert.deepStrictEqual(tokens, [
         {
-            kind: "NumberToken",
-            value: 1.2,
+            kind: "LeftBracketToken",
             startIndex: 0,
+            endIndex: 1,
+        },
+        {
+            kind: "NumberToken",
+            value: 1,
+            startIndex: 1,
+            endIndex: 2,
+        },
+        {
+            kind: "CommaToken",
+            startIndex: 2,
             endIndex: 3,
         },
         {
             kind: "NumberToken",
-            value: 3,
+            value: 2,
+            startIndex: 3,
+            endIndex: 4,
+        },
+        {
+            kind: "RightBracketToken",
             startIndex: 4,
             endIndex: 5,
         },
