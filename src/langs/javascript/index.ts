680ca57363f49d9dd890e6d11d3d1298b21299c8
eeue56 <noah.hall@hey.com>
2026-07-15 00:22:14 +0200

    js: dont do seperate export default

--- src/langs/javascript/index.ts
+++ src/langs/javascript/index.ts
@@ -4,12 +4,6 @@ import { generateProgram } from "./generate.ts";
 import { parse } from "./parser/index.ts";
 import { JsNode, Program } from "./types.ts";
 
-const javascript: Language<Program, JsNode> = {
-    filter: (filterRules, program) => filterProgram(program, filterRules),
-    parse,
-    generate: generateProgram,
-};
-
 /**
  * filters, parser, and generator for working with raw JavaScript, or JavaScript nodes
  *
@@ -17,4 +11,8 @@ const javascript: Language<Program, JsNode> = {
  * @property `generate`: generate JavaScript string from `JsNode`
  * @property `parse`: turn a string into a `JsNode` tree, or string if parsing failed (`Result` type)
  */
-export default javascript;
+export const javascript: Language<Program, JsNode> = {
+    filter: (filterRules, program) => filterProgram(program, filterRules),
+    parse,
+    generate: generateProgram,
+};
